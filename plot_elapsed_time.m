d830ed6066cb6be86a2c472e945aad55b35b6ae2
q89123003 <q89123003@gmail.com>
2021-08-05 01:54:49 -0700

    use different line style

--- plot_elapsed_time.m
+++ plot_elapsed_time.m
@@ -1,26 +1,85 @@
+alg_names = {'original', 'fast'};
+
 trial_num_sweep = round(exp(log(2) :  log(2) : log(512)));
 channel_nums = [4 16 64 256];
 wave_iter_num = 5;
 iterN = 10;
-durations = [2 4];
-wave_num_range = [3 10];
+fs = 500;
+durations = [2 4 8 16];
 
-figure();
-for i_channel_num = 1 : length(channel_nums)
-    channel_num = channel_nums(i_channel_num);
-    for i_duration = 1 : length(durations)
-        subplot(length(channel_nums), length(durations), (i_channel_num - 1) * length(durations) + i_duration);
-        duration = durations(i_duration);
+% colors = cell(1, 2);
+% colors{1} = zeros(length(channel_nums), 3);
+% colors{1}(:, 1) = linspace(1, 0.6, length(channel_nums));
+% colors{1}(:, 2:3) = 0.2;
+% colors{2} = zeros(length(channel_nums), 3);
+% colors{2}(:, 1:2) = 0.2;
+% colors{2}(:, 3) = linspace(1, 0.6, length(channel_nums));
+colors = [1 0 0; 0 0 1];
+markers = {'+', 'x'};
+line_styles = {'-', '--', '-.', ':'};
+
+ytclabels = {};
+ytcs = 10.^(-2:1:7);
+for i = 1 : length(ytcs)
+    ytclabels = [ytclabels ['1e' num2str(log10(ytcs(i)))]];
+end
+
+f = figure();
+f.Position = [100 100 1600 400];
+tiledlayout(1, length(durations));
+for i_duration = 1 : length(durations)
+    duration = durations(i_duration);
+    nexttile;
+    legends = {};
+    ps = [];
+    for i_channel_num = 1 : length(channel_nums)
+        channel_num = channel_nums(i_channel_num);
+        %subplot(length(channel_nums), length(durations), (i_channel_num - 1) * length(durations) + i_duration);
+        
         load(sprintf('./elapsed time results/elapsed_times_dur%d_chn%d.mat', duration, channel_num));
         
         hold on;
         for i_alg = 1 : 2
-            plot(log2(trial_num_sweep), log10(squeeze(mean(elapsed_times(i_alg, :, :), 3))), 'LineWidth', 2);
+            tmp_mean_time = squeeze(mean(elapsed_times(i_alg, :, :), 3));
+            %p = plot(log2(trial_num_sweep), log10(tmp_mean_time) + 3, 'LineWidth', 2, 'Color', colors{i_alg}(i_channel_num, :));
+            p = plot(log2(trial_num_sweep), log10(tmp_mean_time) + 3, ...
+                'LineWidth', 2, 'Color', colors(i_alg, :), 'Marker', markers{i_alg}, 'MarkerSize', 9, 'LineStyle', line_styles{i_channel_num});
+            %fprintf('Alg %d, slope: %f\n', i_alg, ...
+            %    (log10(tmp_mean_time(end)) - log10(tmp_mean_time(1))) ...
+            %    / (log2(trial_num_sweep(end)) - log2(trial_num_sweep(1))));
+            ps = [ps p];
+            legends = [legends sprintf('%s - %d channels', alg_names{i_alg}, channel_num)];
+            
+            slope_sum = 0;
+            for i = 2 : length(tmp_mean_time)
+                slope_sum = slope_sum + (log10(tmp_mean_time(i)) - log10(tmp_mean_time(i - 1))) / (log2(trial_num_sweep(i)) - log2(trial_num_sweep(i - 1)));
+            end
+            slope_mean = slope_sum / (length(tmp_mean_time) - 1);
+            
+            text(9+0.3, log10(tmp_mean_time(end))+3, sprintf('%.2f', slope_mean));
         end
         hold off;
-
-        xlabel('log_2(# of trials)');
-        %xticklabel(exp(log(2) :  log(2) : log(512)));
-        ylabel('elapsed time. log(sec)')
     end
+    
+    title(sprintf('%d time stamps', duration * fs));
+    
+    xlabel('number of trials');
+    xlim([1, 10.5]);
+    xticks(log2(trial_num_sweep));
+    xticklabels(strsplit(num2str(trial_num_sweep)));
+    ylabel('elapsed time (ms)')
+    yticks(-2:1:7);
+%     yticklabels(strsplit(num2str(10.^(-2:1:7))));
+    yticklabels(ytclabels);
+    ylim([-2, 7]);
+    set(gca, 'FontSize', 15);
 end
+
+
+sorted_ps(1:4) = ps(1:2:7);
+sorted_ps(5:8) = ps(2:2:8);
+sorted_legends(1:4) = legends(1:2:7);
+sorted_legends(5:8) = legends(2:2:8);
+lgd = legend(sorted_ps, sorted_legends);
+lgd.Layout.Tile = 'east';
+set(gca, 'FontSize', 15);
